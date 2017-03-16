03221022455aac95957dd8cb6575282d613a310c
Pushkar Lanka <pushkar.lanka@gmail.com>
2017-03-16 16:29:13 -0500

    added single column bootstrap theme

--- star-wars.py
+++ star-wars.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/films')
+def models():
+    return render_template('films.html')
+
+
+@app.route('/characters')
+def models():
+    return render_template('characters.html')
+
+
+@app.route('/planets')
+def models():
+    return render_template('planets.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
