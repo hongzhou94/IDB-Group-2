9fac91d3b742cd63c01c4f3692742062e4db8073
arjunmbajaj <arjunbajaj@Arjuns-MacBook-Pro-3.local>
2017-03-19 17:20:49 -0500

    links WIP

--- db_mock.py
+++ db_mock.py
@@ -8,11 +8,13 @@ import os
 class MockDB:
 
     def __init__(self, static_folder):
+        self.static_planets = [0,1,2]
+        self.static_char = [0,1,2]
+        self.static_films = [0,1,2]
         self.static_folder = static_folder
         self.films = self.init_films()
         self.characters = self.init_characters()
         self.planets = self.init_planets()
-
     def get_film(self, index):
         return self.films[index]
 
@@ -37,9 +39,9 @@ class MockDB:
             data = json.load(f)
 
         d = data['results']
-        f1 = Film(d[0]['title'], d[0]['director'], d[0]['producer'], d[0]['episode_id'], d[0]['release_date'], "http://cdn2us.denofgeek.com/sites/denofgeekus/files/starwars-iv.jpg")
-        f2 = Film(d[1]['title'], d[1]['director'], d[1]['producer'], d[1]['episode_id'], d[1]['release_date'], "https://i.ytimg.com/vi/jDIHiIxUGEY/maxresdefault.jpg")
-        f3 = Film(d[2]['title'], d[2]['director'], d[2]['producer'], d[2]['episode_id'], d[2]['release_date'], "http://static.dolimg.com/lucas/movies/starwars/starwars_epi5_01-de788d5a9549.jpg")
+        f1 = Film(d[0]['title'], d[0]['director'], d[0]['producer'], d[0]['episode_id'], d[0]['release_date'],self.static_char,self.static_planets, "http://cdn2us.denofgeek.com/sites/denofgeekus/files/starwars-iv.jpg")
+        f2 = Film(d[1]['title'], d[1]['director'], d[1]['producer'], d[1]['episode_id'], d[1]['release_date'],self.static_char,self.static_planets, "https://i.ytimg.com/vi/jDIHiIxUGEY/maxresdefault.jpg")
+        f3 = Film(d[2]['title'], d[2]['director'], d[2]['producer'], d[2]['episode_id'], d[2]['release_date'],self.static_char,self.static_planets, "http://static.dolimg.com/lucas/movies/starwars/starwars_epi5_01-de788d5a9549.jpg")
 
         return [f1, f2, f3]
 
@@ -49,9 +51,9 @@ class MockDB:
             data = json.load(f)
 
         d = data['results']
-        c1 = Character(d[0]['name'], d[0]['birth_year'], d[0]['hair_color'], d[0]['height'], d[0]['mass'], "http://starwarscardtraderapp.com/wp-content/uploads/2015/12/99-1-7-Award-Luke-Skywalker.png")
-        c2 = Character(d[1]['name'], d[1]['birth_year'], d[1]['hair_color'], d[1]['height'], d[1]['mass'], "https://lumiere-a.akamaihd.net/v1/images/databank_lobot_01_169_8a50d7ae.jpeg?region=0%2C0%2C1560%2C878&width=768")
-        c3 = Character(d[2]['name'], d[2]['birth_year'], d[2]['hair_color'], d[2]['height'], d[2]['mass'], "http://rcysl.com/wp-content/uploads/2017/03/R2d2-Wallpaper-In-High-Definition-.jpg")
+        c1 = Character(d[0]['name'], d[0]['birth_year'], d[0]['hair_color'], d[0]['height'], d[0]['mass'],self.static_films,self.static_planets, "http://starwarscardtraderapp.com/wp-content/uploads/2015/12/99-1-7-Award-Luke-Skywalker.png")
+        c2 = Character(d[1]['name'], d[1]['birth_year'], d[1]['hair_color'], d[1]['height'], d[1]['mass'],self.static_films,self.static_planets, "https://lumiere-a.akamaihd.net/v1/images/databank_lobot_01_169_8a50d7ae.jpeg?region=0%2C0%2C1560%2C878&width=768")
+        c3 = Character(d[2]['name'], d[2]['birth_year'], d[2]['hair_color'], d[2]['height'], d[2]['mass'],self.static_films,self.static_planets, "http://rcysl.com/wp-content/uploads/2017/03/R2d2-Wallpaper-In-High-Definition-.jpg")
 
         return [c1, c2, c3]
 
@@ -61,8 +63,8 @@ class MockDB:
             data = json.load(f)
 
         d = data['results']
-        p1 = Planet(d[0]['name'], d[0]['climate'], d[0]['population'], d[0]['gravity'], d[0]['terrain'], "https://img.clipartfox.com/92129e5d25a3b8557820a8e286ee002e_chott-el-jerid-wookieepedia-star-wars-tatooine-clipart_1900-815.jpeg")
-        p2 = Planet(d[1]['name'], d[1]['climate'], d[1]['population'], d[1]['gravity'], d[1]['terrain'], "http://overmental.com/wp-content/uploads/2015/07/Naboo-TPM-790x336.jpg")
-        p3 = Planet(d[2]['name'], d[2]['climate'], d[2]['population'], d[2]['gravity'], d[2]['terrain'], "http://cdn.segmentnext.com/wp-content/uploads/2016/05/Star-Wars-Battlefront-Bespin-DLC-1.jpg")
+        p1 = Planet(d[0]['name'], d[0]['climate'], d[0]['population'], d[0]['gravity'], d[0]['terrain'],self.static_films,self.static_char, "https://img.clipartfox.com/92129e5d25a3b8557820a8e286ee002e_chott-el-jerid-wookieepedia-star-wars-tatooine-clipart_1900-815.jpeg")
+        p2 = Planet(d[1]['name'], d[1]['climate'], d[1]['population'], d[1]['gravity'], d[1]['terrain'],self.static_films,self.static_char, "http://overmental.com/wp-content/uploads/2015/07/Naboo-TPM-790x336.jpg")
+        p3 = Planet(d[2]['name'], d[2]['climate'], d[2]['population'], d[2]['gravity'], d[2]['terrain'],self.static_films,self.static_char, "http://cdn.segmentnext.com/wp-content/uploads/2016/05/Star-Wars-Battlefront-Bespin-DLC-1.jpg")
 
         return [p1, p2, p3]
--- models.py
+++ models.py
@@ -1,29 +1,35 @@
 class Film:
-    def __init__(self, title, director, producer, episode_no, release_date, img_url):
+    def __init__(self, title, director, producer, episode_no, release_date, characters, planets, img_url):
         self.title = title
         self.director = director
         self.producer = producer
         self.episode_no = episode_no
         self.release_date = release_date
+        self.characters = characters
+        self.planets = planets
         self.img_url = img_url
 
 
 class Character:
-    def __init__(self, name, birth_year, hair_color, height, mass, img_url):
+    def __init__(self, name, birth_year, hair_color, height, mass, films, planets, img_url):
         self.name = name
         self.birth_year = birth_year
         self.hair_color = hair_color
         self.height = height
         self.mass = mass
+        self.films = films
+        self.planets = planets
         self.img_url = img_url
 
 
 class Planet:
-    def __init__(self, name, climate, population, gravity, terrain, img_url):
+    def __init__(self, name, climate, population, gravity, terrain, films, characters, img_url):
         self.name = name
         self.climate = climate
         self.population = population
         self.gravity = gravity
         self.terrain = terrain
+        self.films = films
+        self.characters = characters
         self.img_url = img_url
 
